c5741aa2656483e00f9f40a59c9b08555a9bedce
Tobaac <tomasbarragan05@gmail.com>
2026-02-27 21:26:57 +0800

    Implement PeopleRecord and heap skeleton

---
+++ src/DatabaseProcessing.java
@@ -0,0 +1,9 @@
+public class DatabaseProcessing{
+
+    public static void main(String[] args) {
+        
+
+    }
+
+
+}
---
+++ src/PeopleRecord.java
@@ -0,0 +1,9 @@
+public class PeopleRecord{
+
+    public static void main(String[] args) {
+        
+
+    }
+
+     
+}
